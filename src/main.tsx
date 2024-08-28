de2e2cf2322340e11bc79916621d09b1c1639fc0
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-08-28 02:06:05 -0400

    add theme to app and set body attribute

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ThemeContext from "./contexts/ThemeContext";
 import App from "./App";
+import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeContext.Provider value={"dark"}>
+      <App />
+    </ThemeContext.Provider>
+  </React.StrictMode>
 );
