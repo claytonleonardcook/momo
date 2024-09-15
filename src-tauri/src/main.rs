24d23b0be77751302267d6b0d6c713a102d2006d
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-09-15 01:13:07 -0400

    make global state for database connection

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,36 +1,71 @@
 // ! Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use rusqlite::{Connection, Result};
-use include_sqlite_sql::{include_sql, impl_sql};
+use std::sync::Mutex;
 
-include_sql!("sql/Person.sql");
+use include_sqlite_sql::{impl_sql, include_sql};
+use rusqlite::{Connection, Result};
+use tauri::Manager;
+
+include_sql!("sql/Artists.sql");
+include_sql!("sql/Albums.sql");
+include_sql!("sql/Tracks.sql");
+include_sql!("sql/Playlists.sql");
+include_sql!("sql/PlaylistTracks.sql");
+
+struct GlobalState {
+    conn: Mutex<Connection>,
+}
 
 #[tauri::command]
-fn greet(name: &str) -> Result<String, String> {
+fn greet(name: &str, state: tauri::State<GlobalState>) -> Result<String, String> {
     if name.is_empty() {
         return Err("Provide a valid name!".into());
     }
 
-    let conn = Connection::open_in_memory().map_err(|_err| "Couldn't connect to database!")?;
+    let conn = state.conn.lock().unwrap();
 
-    conn.create_person_table().map_err(|_err| "Couldn't create person table!")?;
+    conn.get_all_artists(|row| {
+        let name: &str = row.get_ref("name")?.as_str()?;
+        let artist_id: i64 = row.get_ref("id")?.as_i64()?;
 
-    conn.insert_person("Steven").map_err(|_err| "Couldn't insert Steven!")?;
+        println!("{}", name);
 
-    conn.get_all_people(|row| {
-        let name : &str = row.get_ref("name")?.as_str()?;
-
-        print!("{}", name);
+        conn.insert_album("Rocket", artist_id).unwrap();
 
         Ok(())
-    }).map_err(|_err| "Couldn't get all people!")?;
-    
+    })
+    .unwrap();
+
+    conn.get_all_albums(|row| {
+        let name: &str = row.get_ref("name")?.as_str()?;
+
+        println!("{}", name);
+
+        Ok(())
+    })
+    .unwrap();
+
     Ok(format!("Hello, {}! You've been greeted from Rust!", name))
 }
 
 fn main() -> Result<()> {
     tauri::Builder::default()
+        .setup(|app| {
+            let conn = Connection::open_in_memory().unwrap();
+
+            conn.create_artists_table().unwrap();
+            conn.create_albums_table().unwrap();
+            conn.create_tracks_table().unwrap();
+
+            conn.insert_artist("Alex G").unwrap();
+
+            app.manage(GlobalState {
+                conn: Mutex::new(conn),
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
