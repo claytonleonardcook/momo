7cccba2c810b63baa85412c1f262bcd58dd8b29a
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-09-15 01:12:50 -0400

    write new queries for general structure

---
+++ src-tauri/sql/Albums.sql
@@ -0,0 +1,32 @@
+-- name: create_albums_table
+-- Creates the albums table
+CREATE TABLE IF NOT EXISTS Albums (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    artist_id INTEGER NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES Artists (id) ON DELETE CASCADE
+)
+
+-- name: insert_album
+-- Inserts a album into the albums table
+-- param: name: &str - name of album
+-- param: artist_id: i64 - id of parent artist
+INSERT INTO Albums (name, artist_id)
+    VALUES (:name, :artist_id)
+
+-- name: get_all_albums?
+-- Get's all albums from albums table
+SELECT id, name, artist_id FROM Albums
+
+-- name: get_albums_by_artist
+-- Get all albums for a specific artist
+-- param: artist_id: i64 - the id of the artist
+SELECT id, name, artist_id
+    FROM Albums
+    WHERE artist_id = :artist_id;
+
+-- name: delete_album
+-- Deletes an album by id
+-- param: album_id: i64 - the id of the album to delete
+DELETE FROM Albums
+    WHERE id = :album_id;
---
+++ src-tauri/sql/Tracks.sql
@@ -0,0 +1,43 @@
+-- name: create_tracks_table
+-- Creates the tracks table
+CREATE TABLE IF NOT EXISTS Tracks (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    path TEXT NOT NULL,
+    album_id INTEGER NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Albums (id) ON DELETE CASCADE
+)
+
+-- name: insert_track
+-- Inserts a track into the tracks table
+-- param: name: &str - name of track
+-- param: path: &str - real path to track file
+-- param: path: i64 - id of parent album
+INSERT INTO Tracks (name, path, album_id)
+    VALUES (:name, :path, :album_id)
+
+-- name: get_all_tracks?
+-- Get's all tracks from tracks table
+SELECT id, name, album_id FROM Tracks
+
+-- name: get_tracks_by_artist
+-- Get all tracks for a specific artist
+-- param: artist_id: i64 - the id of the artist
+SELECT Tracks.id, Tracks.name, Tracks.album_id
+    FROM Tracks
+    JOIN Albums ON Tracks.album_id = Albums.id
+    JOIN Artists ON Albums.artist_id = Artists.id
+    WHERE Artists.id = :artist_id;
+
+-- name: get_tracks_by_album
+-- Get all tracks for a specific album
+-- param: album_id: i64 - the id of the album
+SELECT id, name, path, album_id
+    FROM Tracks
+    WHERE album_id = :album_id;
+
+-- name: delete_track
+-- Deletes a track by id
+-- param: track_id: i64 - the id of the track to delete
+DELETE FROM Tracks
+    WHERE id = :track_id;
