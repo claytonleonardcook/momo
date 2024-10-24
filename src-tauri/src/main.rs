d56f9ea6f21f348390b70fcfa72beac08f1855a6
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-10-24 01:54:39 -0400

    rename album file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // ! Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod albums;
+mod album;
 mod track;
 
 use std::sync::Mutex;
