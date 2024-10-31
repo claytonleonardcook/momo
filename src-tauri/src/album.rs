41067ad45b557a1d02c91e1309154df16becfaaf
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-10-31 11:12:17 -0400

    add get albums by artist endpoint

--- src-tauri/src/album.rs
+++ src-tauri/src/album.rs
@@ -47,6 +47,27 @@ pub fn get_all_albums(state: &GlobalState) -> Result<Vec<Album>, String> {
     Ok(albums.to_vec())
 }
 
+#[tauri::command]
+pub fn get_albums_by_artist(artist_id: i64, state: &GlobalState) -> Result<Vec<Album>, String> {
+    let connnection = state.connection.lock().unwrap();
+
+    let albums = &mut Vec::new();
+
+    connnection
+        .get_albums_by_artist(artist_id, |row| {
+            let id: i64 = row.get_ref("id")?.as_i64()?;
+            let name: String = row.get_ref("name")?.as_str()?.to_string();
+            let artist_id: i64 = row.get_ref("artist_id")?.as_i64()?;
+
+            albums.push(Album::new(id, name, artist_id));
+
+            Ok(())
+        })
+        .unwrap();
+
+    Ok(albums.to_vec())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,6 +104,40 @@ mod tests {
         }
 
         let albums = get_all_albums(&state).unwrap();
+
+        assert_eq!(
+            format!("{:?}", albums.to_vec()),
+            r##"[Album { id: 1, name: "Rocket", artist_id: 1 }, Album { id: 2, name: "Trick", artist_id: 1 }]"##,
+        );
+    }
+
+    #[test]
+    fn can_get_albums_by_artist() {
+        let state = GlobalState {
+            connection: Mutex::new(Connection::open_in_memory().unwrap()),
+        };
+
+        {
+            let connection = state.connection.lock().unwrap();
+
+            connection.create_artists_table().unwrap();
+            connection.create_albums_table().unwrap();
+            connection.create_tracks_table().unwrap();
+
+            let aritst_id = connection
+                .insert_artist("Alex G", |row| Ok(row.get_ref("id")?.as_i64()?))
+                .unwrap();
+
+            connection
+                .insert_album("Rocket", aritst_id, |_row| Ok(()))
+                .unwrap();
+
+            connection
+                .insert_album("Trick", aritst_id, |_row| Ok(()))
+                .unwrap();
+        }
+
+        let albums = get_albums_by_artist(1, &state).unwrap();
 
         assert_eq!(
             format!("{:?}", albums.to_vec()),
