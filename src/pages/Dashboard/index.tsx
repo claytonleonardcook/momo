a4afbe509ad779e974a00888983da61f4bff41c3
Clayton Cook <claytonleonardcook+github@gmail.com>
2024-11-14 00:35:15 -0500

    remove boiler plate code from dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import Button from "@/components/atoms/Button";
+import { invoke } from "@tauri-apps/api/core";
 import styles from "./style.module.scss";
-import { useToast } from "@/hooks/useToast";
+import TrackControls from "@/components/organisms/TrackControls";
+import MediaGrid from "@/components/organisms/MediaGrid";
 
 function Dashboard() {
-  const { addToast } = useToast();
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  function greet() {
-    addToast("Greeting you :)", "blue");
-
-    invoke("greet", { name })
-      .then((message) => setGreetMsg(message as string))
-      .catch(console.error);
-  }
+  invoke("get_all_tracks").then(console.log).catch(console.error);
 
   return (
     <main className={styles["dashboard"]}>
-      <h1>Welcome to Tauri!</h1>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <Button type={"submit"}>Greet</Button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <TrackControls trackDuration={50} />
+      <MediaGrid />
     </main>
   );
 }
